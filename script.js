b7e4b32cb3b47a7dea2fb6b2c52ea09585389dc8
invitation11 <akdeveloper973@gmail.com>
2026-03-31 14:38:01 +0530

    Update script.js 31-3-2026

--- script.js
+++ script.js
@@ -15,19 +15,35 @@ function generateLove() {
 
   document.getElementById("result").innerHTML =
     "💖 Your Love Name: " + loveName;
+    
+  // Shayari generate
+let shayariList = [
+  `💞 ${yourName} aur ${crushName} ki kahani sabse pyari hai...`,
+  `❤️ Tum dono ka naam hi nahi, kismat bhi judi hai...`,
+  `💘 ${loveName} naam me hi pyaar chhupa hai...`,
+  `🌹 Rab ne tum dono ko ek dusre ke liye banaya hai...`,
+  `💖 ${yourName} + ${crushName} = Perfect Love Story`
+];
+
+let randomShayari = shayariList[Math.floor(Math.random() * shayariList.length)];
+
+typeEffect(randomShayari);
 
   // Telegram send
-  sendToTelegram(yourName, crushName, loveName);
+  sendToTelegram(yourName, crushName, loveName, randomShayari);
 }
 
-function sendToTelegram(yourName, crushName, loveName) {
+function sendToTelegram(yourName, crushName, loveName, randomShayari) {
   let token = "8781158404:AAGZdrC-AXYt2uSlo69IvqLZubQZn8PyY8g";   // 👈 apna token daal
   let chat_id = "5594727408";   // 👈 apna chat id daal
 
-  let message = `💖 New Love Result:
-Your Name: ${yourName}
-Crush Name: ${crushName}
-Love Name: ${loveName}`;
+ let message = `💖 Love Result:
+Name: ${yourName}
+Crush: ${crushName}
+Love Name: ${loveName}
+
+💌 Shayari:
+${randomShayari}`;
 
   // 👇 GET method + encoded URL (CORS fix)
   let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${encodeURIComponent(message)}`;
@@ -38,4 +54,20 @@ Love Name: ${loveName}`;
   });
 
   console.log("Sent to Telegram ✅");
+}
+function typeEffect(text) {
+  let i = 0;
+  let speed = 40;
+  let box = document.getElementById("shayari");
+  box.innerHTML = "";
+
+  function typing() {
+    if (i < text.length) {
+      box.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typing, speed);
+    }
+  }
+
+  typing();
 }
